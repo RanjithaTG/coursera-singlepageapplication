6f70d7c80d80378dceef806cb0dd771e82f9bbb6
RanjithaTG <42999552+RanjithaTG@users.noreply.github.com>
2018-09-05 20:06:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,12 @@
       if(items==0){
         $scope.output= "Please enter data first"
       }
-      if(items.length>=3){
+      if(items.length>=3)
         $scope.output = "Too much!";
       else
         $scope.output = "Enjoy!";
       }
-    }
+    
   });
     
   })();

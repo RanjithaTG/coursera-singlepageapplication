b8e924710fb5ed4ef77ae2e9da5a4ae186395ece
RanjithaTG <42999552+RanjithaTG@users.noreply.github.com>
2018-09-06 21:48:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,20 @@
 			'LunchCheckController',
 			function($scope) {
 				var items = '';
-				var foodItems='';
 				$scope.checkFoodCount = function() {
 					if (angular.isDefined($scope.userInput)
 							&& $scope.userInput != ""){
 						items = $scope.userInput.split(',');
 						for(var i=0;i<=items.length;i++){
-							if(items[i]!==""){
-								foodItems[i] = items[i];
+							if(items[i] == ""){
+								items.splice(i,1);
 							}
 						}
 					}
-					if (foodItems.length == 0) {
+					if (items.length == 0) {
 						$scope.output = "Please enter data first"
 					} else {
-						if (foodItems.length > 3) {
+						if (items.length > 3) {
 							$scope.output = "Too much!";
 						} else {
 							$scope.output = "Enjoy!";

238a6deeed18f9e1f287c6df1de3a1bd68fc378e
RanjithaTG <42999552+RanjithaTG@users.noreply.github.com>
2018-09-06 20:54:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-(function (){
-  'use strict';
-  
-  
-  angular.module("LunchCheck" ,[]).
-  controller('LunchCheckController',function ($scope){
-    $scope.checkFoodCount = function(){
-      if( angular.isDefined($scope.userInput) && $scope.userInput != "")
-         var items = $scope.userInput.split(',');
-      if(angular.isUndefined(items) && items.length==0){
-        $scope.output= "Please enter data first"
- 	$scope.success=false;
-      }
-      else{
-            if(items.length>3){
-              $scope.output = "Too much!";
-              $scope.success=false;
-            }
-            else{
-              $scope.output = "Enjoy!";
-               $scope.success=true;
-            }
-    }
-	}
-    
-  });
-    
-  })();
+(function() {
+	'use strict';
+
+	angular.module("LunchCheck", []).controller(
+			'LunchCheckController',
+			function($scope) {
+				var items = '';
+				$scope.checkFoodCount = function() {
+					if (angular.isDefined($scope.userInput)
+							&& $scope.userInput != "")
+						items = $scope.userInput.split(',');
+					if (angular.isUndefined(items) && items.length == 0) {
+						$scope.output = "Please enter data first"
+						$scope.success = false;
+					} else {
+						if (items.length > 3) {
+							$scope.output = "Too much!";
+							$scope.success = false;
+						} else {
+							$scope.output = "Enjoy!";
+							$scope.success = true;
+						}
+					}
+				}
+
+			});
+
+})();

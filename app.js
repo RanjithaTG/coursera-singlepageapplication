dd5403aa308442ef3fe1afc450e15b013def6a34
RanjithaTG <42999552+RanjithaTG@users.noreply.github.com>
2018-09-06 21:34:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,14 @@
 				var items = '';
 				$scope.checkFoodCount = function() {
 					if (angular.isDefined($scope.userInput)
-							&& $scope.userInput != "")
+							&& $scope.userInput != ""){
 						items = $scope.userInput.split(',');
+						for(var i=0;i< items.length;i++){
+							if(items[i]==""){
+								items.splice(i,1);
+							}
+						}
+					}
 					if (items.length == 0) {
 						$scope.output = "Please enter data first"
 					} else {
